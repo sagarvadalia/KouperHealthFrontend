3e42faa8f3076909f5c3fc099d8751e5f5261a54
sagar vadalia <sagarvadalia@sagars-MacBook-Pro.local>
2024-12-30 10:16:50 -0500

    react-router boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { BrowserRouter } from 'react-router'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+      <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+      
+   
+   
     <App />
+    <ReactQueryDevtools initialIsOpen={false} />
+    </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
