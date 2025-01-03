6bbd0b7dcc73ff30dca6662abce8948f95df3e4a
sagar vadalia <sagarvadalia@sagars-MBP.fios-router.home>
2025-01-03 11:45:13 -0500

    patient view

--- src/patients/PatientView.tsx
+++ src/patients/PatientView.tsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router";
 import { useGetPatient } from "./hooks/useGetPatient";
+import { Typography } from "@mui/material";
 
 export const PatientView = () => {
   const { id } = useParams();
@@ -8,10 +9,17 @@ export const PatientView = () => {
   if (error) return <div>Error: {error.message}</div>;
 
   const { patient } = data!;
-
+//   TODO: build out some valuable patient page
   return (
     <div>
-      <p>{patient?.name}</p>
+      <Typography variant="h6">Patient Page for{patient?.name}</Typography>
+      <Typography variant="body1">Epic ID: {patient?.epicId}</Typography>
+      <Typography variant="body1">Phone Number: {patient?.phoneNumber}</Typography>
+      <Typography variant="body1">Attending Physician: {patient?.attendingPhysician}</Typography>
+      <Typography variant="body1">Discharge Date: {patient?.dischargeDate}</Typography>
+      <Typography variant="body1">Primary Care Provider: {patient?.primaryCareProvider}</Typography>
+      <Typography variant="body1">Insurance: {patient?.insurance}</Typography>
+      <Typography variant="body1">Disposition: {patient?.disposition}</Typography>
     </div>
   );
 };
